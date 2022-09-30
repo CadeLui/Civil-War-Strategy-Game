78daa576e7287ddbfc59733848dd0f6f181d2434
CadeLui <videogamefpslover@gmail.com>
2022-09-29 22:11:03 -0400

    Added a toString method to ObjectMatrix

--- map/ObjectMatrix.h
+++ map/ObjectMatrix.h
@@ -8,26 +8,43 @@
 class ObjectMatrix
 {
 	private:
-		std::vector<std::vector<ObjectHolder*>*> objectVector;
+		std::vector<std::vector<ObjectHolder*>> objectVector;
 	public:
 		ObjectMatrix(int rows, int cols)
 		{
+			objectVector.resize(rows);
 			for (int r = 0; r < rows; r++)
 			{
-				objectVector.push_back(new std::vector<ObjectHolder*>);
-				for (int c = 0; c < cols; r++)
-					objectVector[r]->push_back(new ObjectHolder());
+				objectVector[r].resize(cols);
+				for (int c = 0; c < cols; c++)
+				{
+					objectVector[r][c] = new ObjectHolder();
+				}
 			}
 		}
 		void placeObject(void* newObj, std::string newSym, int r, int c)
 		{
-			objectVector[r]->at(c) = new ObjectHolder(newObj, newSym);
+			objectVector[r][c] = new ObjectHolder(newObj, newSym);
 		}
 		int pickAndPlace(int r1, int c1, int r2, int c2)
 		{
-			if (!objectVector[r2]->at(c2)->getObject()) return 1;
+			if (!objectVector[r2][c2]->getObject()) return 1;
 			objectVector[r2][c2] = objectVector[r1][c1];
-			objectVector[r1]->at(c2) = new ObjectHolder();
+			objectVector[r1][c2] = new ObjectHolder();
 			return 0;
+		}
+		std::string toString()
+		{
+			std::string output;
+			for (int r = 0; r < objectVector.size(); r++)
+			{
+				output += "| ";
+				for (int c = 0; c < objectVector.size(); c++)
+				{
+					output += objectVector[r][c]->getSymbol() + " ";
+				}
+				output += "|\n";
+			}
+			return output;
 		}
 };
